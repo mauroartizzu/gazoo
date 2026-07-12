694c7fbf4b38780dd9f3b5aaf8607b44f647e851
Mauro Artizzu <mauro.artizzu@gmail.com>
2026-07-12 09:29:46 +0200

    Add RelayNotifier wrapping RelayServiceHandle for testable UI state

---
+++ lib/ui/state/relay_notifier.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+
+import '../../core/config/server_config.dart';
+import '../../core/relay/relay_service.dart';
+
+class RelayNotifier extends ChangeNotifier {
+  final RelayServiceHandle Function() createRelayService;
+
+  RelayServiceHandle? _service;
+  StreamSubscription<RelayEvent>? _subscription;
+  ServerConfig? _activeServer;
+  RelayEvent? _lastEvent;
+
+  RelayNotifier({RelayServiceHandle Function()? createRelayService})
+      : createRelayService = createRelayService ?? (() => RelayService());
+
+  ServerConfig? get activeServer => _activeServer;
+  RelayEvent? get lastEvent => _lastEvent;
+  bool get isRunning => _service != null;
+
+  Future<void> start(ServerConfig server) async {
+    if (_service != null) {
+      throw StateError('RelayNotifier already running; call stop() first');
+    }
+    final service = createRelayService();
+    _service = service;
+    _activeServer = server;
+    _lastEvent = null;
+    _subscription = service.events.listen((event) {
+      _lastEvent = event;
+      notifyListeners();
+    });
+    notifyListeners();
+    await service.start([server]);
+  }
+
+  Future<void> stop() async {
+    final service = _service;
+    if (service == null) return;
+    await _subscription?.cancel();
+    _subscription = null;
+    await service.dispose();
+    _service = null;
+    _activeServer = null;
+    _lastEvent = null;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    unawaited(stop());
+    super.dispose();
+  }
+}
